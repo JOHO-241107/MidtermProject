1137b132a462fa2e182360788325a8e63b799ad9
HoriMasataka <241107@std.hi-joho.ac.jp>
2025-06-20 12:27:32 +0900

    中間課題のプログラム　エラーなし

---
+++ src/Character.java
@@ -0,0 +1,17 @@
+public abstract class Character implements Creature{
+    String name;
+    int hp;
+
+    public Character(String name, int hp) {
+        this.name = name;
+        this.hp = hp;
+    }
+
+    public final boolean isAlive(){
+        return this.hp > 0;
+    }
+
+    public void showStats() {
+        System.out.println(this.name + "：HP " + this.hp);
+    }
+}
---
+++ src/GameMaster.java
@@ -0,0 +1,29 @@
+public class GameMaster {
+    public static void main(String[] args) {
+        Hero abel = new Hero("アベル", 100, "銅の剣");
+        Slime slm = new Slime("スライムA", 30);
+        System.out.println("冒険が始まる...!");
+        abel.showStats();
+        slm.showStats();
+        System.out.println("戦闘開始！");
+        abel.attack(slm);
+        slm.showStats();
+        slm.attack(abel);
+        abel.showStats();
+        abel.heal();
+        abel.showStats();
+        abel.attack(slm);
+        abel.attack(slm);
+        abel.attack(slm);
+        System.out.println("--- 戦闘終了 ---");
+        abel.showStats();
+        slm.showStats();
+        System.out.println(abel.name + "は生きている： " + abel.isAlive());
+        System.out.println(slm.name + "は生きている： " + slm.isAlive());
+        System.out.println("--- 参照の確認 ---");
+        Character partyMenber = abel;
+        partyMenber.hp -= 50;
+        System.out.println("partyMenber経由でHPを減らした後：");
+        abel.showStats();
+    }
+}
